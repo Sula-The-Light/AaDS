e34eca008ff132baf0ca2a8ca677cfd7b23ebfa3
Sultan <esmagzam66@gmail.com>
2026-03-20 17:50:55 +0500

    init commit

--- src/Task3.java
+++ src/Task3.java
@@ -1,2 +1,12 @@
+// Yesmagzam Sultan IT-2502
 public class Task3 {
+    public static boolean isPrime(int n, int i) {
+        if (n <= 2) return n == 2;
+        if (n % i == 0 || i * i > n) return n % i != 0;
+        return isPrime(n, i + 1);
+    }
+    public static void main(String[] args) {
+        int n = 7;
+        System.out.println(isPrime(n, 2) ? "Prime" : "Composite");
+    }
 }
